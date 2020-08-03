1c05904b7c778ae2247edd8f6c1c6d595e8bcca0
ChitrangGoyani <chitranggoyani@gmail.com>
2020-08-03 14:40:46 +0530

    added textfields for adding transactions wout floating button

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class PEApp extends StatelessWidget {
 class PEHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final titleController = TextEditingController();
+    final amtController = TextEditingController();
     final List<Transaction> transactions = [
       Transaction(
         id: '001',
@@ -33,7 +35,7 @@ class PEHomePage extends StatelessWidget {
         title: Text('Expenses Tracker'),
       ),
       body: Column(
-       // crossAxisAlignment: CrossAxisAlignment.end,
+        // crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -55,6 +57,24 @@ class PEHomePage extends StatelessWidget {
               ),
             ),
           ),
+          Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.symmetric(horizontal: 3, vertical: 2),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(labelText: 'Enter title')),
+                TextField(
+                    controller: amtController,
+                    decoration: InputDecoration(labelText: 'Enter amount')),
+                FlatButton(child: Text('Add Transaction', style: TextStyle(color: Colors.white),),onPressed : () {
+                  print(titleController.text);
+                  print(amtController.text);
+                }, color: Colors.black)
+              ],
+            ),
+          ),
           Column(
             children: transactions.map((tx) {
               return Container(
@@ -62,47 +82,54 @@ class PEHomePage extends StatelessWidget {
                   height: 50,
                   margin: EdgeInsets.only(left: 10, right: 10),
                   child: Card(
-                    shadowColor: Colors.grey[200],   
+                    shadowColor: Colors.grey[200],
                     child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.green,
-                            width: 1,
-                          )
-                        ),
-                       // margin: EdgeInsets.all(5),
-                       // padding: EdgeInsets.all(5),
-                       // alignment: Alignment.centerLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Text(
-                             '\$' + tx.amt.toString(),
-                          //  textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.green, fontSize: 25, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
                           Container(
-                            child: Text(
-                              tx.name,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                              color: Colors.green,
+                              width: 1,
+                            )),
+                            // margin: EdgeInsets.all(5),
+                            // padding: EdgeInsets.all(5),
+                            // alignment: Alignment.centerLeft,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Text(
+                                '\$' + tx.amt.toString(),
+                                //  textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.green,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold),
+                              ),
                             ),
                           ),
-                          Container(
-                              child: Text(DateFormat().add_yMMMd().format(tx.date),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  tx.name,
                                   textAlign: TextAlign.center,
-                                  style: TextStyle(color: Colors.grey),
-                              ))],
-                      )
-                    ]),
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Container(
+                                  child: Text(
+                                DateFormat().add_yMMMd().format(tx.date),
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.grey),
+                              ))
+                            ],
+                          )
+                        ]),
                   ));
             }).toList(),
           )
